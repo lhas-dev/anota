e6821e8b7332a5c0143f1698373e984aa0b6d686
Luiz Almeida (lhas) <luizhrqas@gmail.com>
2026-03-22 19:15:49 -0300

    redirect to dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   layout "home"
   allow_unauthenticated_access
 
-  def index; end
+  def index
+    redirect_to dashboard_path if authenticated?
+  end
 end
